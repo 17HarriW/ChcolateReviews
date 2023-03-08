78a11a8f3a585a06d0ac10dfa2a9230e651b6253
William Harris <17HarriW@fulford.york.sch.uk>
2023-03-08 12:27:21 +0000

    Added default values

--- CreateDatabase.sql
+++ CreateDatabase.sql
@@ -1,23 +1,26 @@
-﻿CREATE TABLE ChocolateBars
+﻿-- Create the chocolate bar table
+CREATE TABLE ChocolateBars
 (
 	[ChocolateBarID] INT NOT NULL PRIMARY KEY, 
     [Name] NCHAR(128) NOT NULL, 
     [Price] SMALLMONEY NOT NULL
 )
 
+-- Create the review table
 CREATE TABLE Reviews
 (
 	[ReviewID] INT NOT NULL PRIMARY KEY, 
     [ChocolateBarID] INT NOT NULL, 
     [UserID] INT NOT NULL, 
-    [Score] INT NOT NULL, 
-    [Comment] TEXT NOT NULL
+    [Score] INT NOT NULL DEFAULT 0, 
+    [Comment] TEXT NOT NULL DEFAULT 'No text added'
 )
 
+-- Create the user table
 CREATE TABLE Users
 (
 	[UserID] INT NOT NULL PRIMARY KEY, 
-    [FirstName] NCHAR(128) NOT NULL, 
-    [LastName] NCHAR(128) NOT NULL
+    [FirstName] NCHAR(128) NOT NULL DEFAULT '', 
+    [LastName] NCHAR(128) NOT NULL DEFAULT ''
 )
 
