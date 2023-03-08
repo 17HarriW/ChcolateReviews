f0e6dfd847e677237f2ad5961819b4f05588c67e
William Harris <17HarriW@fulford.york.sch.uk>
2023-03-08 12:24:07 +0000

    Added database fields

---
+++ CreateDatabase.sql
@@ -0,0 +1,23 @@
+﻿CREATE TABLE ChocolateBars
+(
+	[ChocolateBarID] INT NOT NULL PRIMARY KEY, 
+    [Name] NCHAR(128) NOT NULL, 
+    [Price] SMALLMONEY NOT NULL
+)
+
+CREATE TABLE Reviews
+(
+	[ReviewID] INT NOT NULL PRIMARY KEY, 
+    [ChocolateBarID] INT NOT NULL, 
+    [UserID] INT NOT NULL, 
+    [Score] INT NOT NULL, 
+    [Comment] TEXT NOT NULL
+)
+
+CREATE TABLE Users
+(
+	[UserID] INT NOT NULL PRIMARY KEY, 
+    [FirstName] NCHAR(128) NOT NULL, 
+    [LastName] NCHAR(128) NOT NULL
+)
+
